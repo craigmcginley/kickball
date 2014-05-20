115c9b5626e6649ceec13594b6b68675d9f68304
craigmcginley <craigmcginley@gmail.com>
2014-05-20 12:10:09 -0400

    Created team template   *need to add links   *can we share variables or define to use in multiple 'gets'

--- server.rb
+++ server.rb
@@ -1,6 +1,42 @@
 require 'sinatra'
+require 'csv'
+
+
+
+
 
 get '/' do
-  @teams = ['betty', 'boop']
+  @players = []
+
+  CSV.foreach('public/lackp_starting_rosters.csv', headers: true) do |row|
+
+     player = {
+      first_name: row["first_name"],
+      last_name: row["last_name"],
+      position: row["position"],
+      team: row["team"]
+    }
+
+    @players << player
+  end
   erb :index
+end
+
+get '/teams/:team' do
+   @players = []
+
+  CSV.foreach('public/lackp_starting_rosters.csv', headers: true) do |row|
+
+     player = {
+      first_name: row["first_name"],
+      last_name: row["last_name"],
+      position: row["position"],
+      team: row["team"]
+    }
+
+    @players << player
+  end
+
+  @team = params[:team]
+  erb :team
 end
