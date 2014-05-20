9fff51e07b472fa8ab471b4eb45b0480ece5eee4
craigmcginley <craigmcginley@gmail.com>
2014-05-20 13:23:16 -0400

    Fixed local variable scope in server.rb

--- server.rb
+++ server.rb
@@ -1,42 +1,30 @@
 require 'sinatra'
 require 'csv'
+require 'uri'
 
+players = []
+CSV.foreach('public/lackp_starting_rosters.csv', headers: true) do |row|
+
+   player = {
+    first_name: row["first_name"],
+    last_name: row["last_name"],
+    position: row["position"],
+    team: row["team"]
+  }
+
+  players << player
+end
 
 
 
 
 get '/' do
-  @players = []
-
-  CSV.foreach('public/lackp_starting_rosters.csv', headers: true) do |row|
-
-     player = {
-      first_name: row["first_name"],
-      last_name: row["last_name"],
-      position: row["position"],
-      team: row["team"]
-    }
-
-    @players << player
-  end
+  @players = players
   erb :index
 end
 
 get '/teams/:team' do
-   @players = []
-
-  CSV.foreach('public/lackp_starting_rosters.csv', headers: true) do |row|
-
-     player = {
-      first_name: row["first_name"],
-      last_name: row["last_name"],
-      position: row["position"],
-      team: row["team"]
-    }
-
-    @players << player
-  end
-
+  @players = players
   @team = params[:team]
   erb :team
 end
